02188f4f6bbab5929024427bf4c5b82e464232b8
RaviKumar046 <24ravi46@gmail.com>
2025-08-31 18:30:46 +0000

    data type Conversion

---
+++ 01_Basic/05_dataTypeSummary.js
@@ -0,0 +1,12 @@
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp =null
+ let userEmail; 
+
+ const id=symbol('123')
+ const anotherId=symbol('123')
+console.log(id===anotherId);
+
+const  bigNumber=7324589732239n
