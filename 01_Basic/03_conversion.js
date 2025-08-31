ca145a5b16440a1426815400d2431a8c92d5a6d3
RaviKumar046 <24ravi46@gmail.com>
2025-08-31 17:45:28 +0000

    conversion of data-Type

---
+++ 01_Basic/03_conversion.js
@@ -0,0 +1,11 @@
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+let score ="ravi"
+ let valueInNumber=Number(score)
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+ 
